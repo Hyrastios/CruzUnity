f20de94711acdcd924144819c0fd9cc2bc3c8f15
Richard BRUNEAU <bruneau.rich@gmail.com>
2019-02-26 18:23:33 +0100

    Là je fais un commit en tapant du texte voir si ça marche

---
+++ Jeu d'avions/Assets/Assets/Script/Script_Bouee.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+
+// NOTE : Il y a de grandes chances pour que la force de flotaison soit inutile.  
+
+public class ForceDeFlotaison : MonoBehaviour
+{
+
+    /*
+    bool danse = false;
+
+    public float declechement = 1;
+    private float countdown = 2f;
+
+    public int force = 50;
+    public float delai = 3f; 
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(buoyancySimulation());
+    } 
+
+    /*
+    IEnumerator buoyancySimulation()
+    {
+        while (danse)
+        {
+            yield return new WaitForSeconds(delai);
+            GetComponent
+            <Rigidbody>().AddForce(Vector2.up * force);
+        }
+    }
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*
+        if(Time.deltaTime == declechement)
+        {
+            danse = true;
+        }
+        */
+    }
+}
+
+
