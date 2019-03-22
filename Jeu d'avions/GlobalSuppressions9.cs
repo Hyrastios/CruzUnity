0d698f69509326d432591346498ddae0fd931719
Richard BRUNEAU <bruneau.rich@gmail.com>
2019-03-22 18:20:40 +0100

    Osef de ces fichiers..

---
+++ Jeu d'avions/GlobalSuppressions9.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Ajouter un modificateur readonly", Justification = "<En attente>", Scope = "member", Target = "~F:Checkpoint.d2")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Styles d'affectation de noms", Justification = "<En attente>", Scope = "member", Target = "~P:Checkpoint.porteTotale")]
+
