537db9b918f5356d2ccc35d2dba5efc3996b27c7
Richard BRUNEAU <bruneau.rich@gmail.com>
2019-04-01 10:02:51 +0200

    idk Sorry

---
+++ idk/Assets/Assets/Script/Jeu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jeu : MonoBehaviour
+{
+    private int NbPorteTotale { get; set; }
+    private int NbPorteFranchie;
+
+    Porte[] TabPorte = new Porte[8];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NbPorteFranchie = 0;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+     public void SetNbPorteTotale( int newNbPorteTotale) { this.NbPorteTotale = newNbPorteTotale; }
+     public void SetNbPorteFranchie() { this.NbPorteFranchie ++ ; }
+}
+
