8f2f96268d55c50ab21e8073a44fe9c27fbfa36a
Richard BRUNEAU <bruneau.rich@gmail.com>
2019-04-02 17:15:45 +0200

    Appelle à la fonction qui immobilize l'avion (ne marche pas)

--- idk/Assets/Assets/Script/Deathzone.cs
+++ idk/Assets/Assets/Script/Deathzone.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Aeroplane;
 
 public class Deathzone : MonoBehaviour
 {
@@ -11,8 +12,17 @@ public class Deathzone : MonoBehaviour
     {
         if (other.transform.gameObject.tag == "Player")
         {
-
+            if (other.gameObject.GetComponent<AeroplaneController>() != null)
+            {
+                other.gameObject.GetComponent<AeroplaneController>().Immobilize();
+                Debug.Log("Normalement t'as perdu le contrôle là");
+            }
+            else
+            {
+                Debug.Log("Le pointeur est null");
+            }
         }
     }
+
 }
 
