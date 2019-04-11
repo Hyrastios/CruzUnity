57f1ccba1093b287cbffd5f47247c3b036ac5617
amelyR <amelie.riche01@gmail.com>
2019-04-11 09:24:09 +0200

    rm

--- Jeu d'avions/GlobalSuppressions2.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0093:Convertir « if » en expression « && »", Justification = "<En attente>", Scope = "member", Target = "~M:UnityStandardAssets.Water.WaterBase.UpdateShader")]
-
--- Jeu d'avions/GlobalSuppressions3.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0018:Comparaison des nombres à virgule flottante avec l’opérateur d’égalité", Justification = "<En attente>", Scope = "member", Target = "~M:UnityStandardAssets.Vehicles.Aeroplane.Fly.Update")]
-
