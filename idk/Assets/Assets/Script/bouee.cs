9a2f0e4cc28afba608dbd7111e7173b202067cf7
Richard BRUNEAU <bruneau.rich@gmail.com>
2019-04-20 17:45:49 +0200

    Update bouee.cs

--- idk/Assets/Assets/Script/bouee.cs
+++ idk/Assets/Assets/Script/bouee.cs
@@ -3,20 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using System; 
 
-// NOTE : Il y a de grandes chances pour que la force de flotaison soit inutile.  
-
 public class Bouee : MonoBehaviour
 {
     public float delai = 1f;
-   
-    /*
-    bool danse = false;
-
-    public float declechement = 1;
-    private float countdown = 2f;
-
-    public int force = 50;
-    */
 
     bool rotationActivee = true;
     public float uneVariable = 50.0f; 
@@ -25,24 +14,9 @@ public class Bouee : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Pour la partie haut/bas
-        //StartCoroutine(buoyancySimulation());
-
         // Pour la partie rotation 
         StartCoroutine(oscilation());
     } 
-
-    /*
-    IEnumerator buoyancySimulation()
-    {
-        while (danse)
-        {
-            yield return new WaitForSeconds(delai);
-            GetComponent
-            <Rigidbody>().AddForce(Vector2.up * force);
-        }
-    }
-    */
 
     IEnumerator oscilation()
     {
@@ -53,15 +27,6 @@ public class Bouee : MonoBehaviour
         }
     }
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        /* if(Time.deltaTime == declechement)
-        {
-            danse = true;
-        } */
-    }
 }
 
 
